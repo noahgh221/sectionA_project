9cb458036b8436ce026796b7eab2fe6b1f0b9567
Noah Huffman <noah.huffman@gmail.com>
2017-11-28 10:16:50 -0500

    fix export_options

--- as_seca_publish_update_eads.py
+++ as_seca_publish_update_eads.py
@@ -46,7 +46,7 @@ print "Authenticated!"
 headers = {'X-ArchivesSpace-Session':session}
 
 #set EAD export options: number components and include DAOs
-export_options = '?numbered_cs=true&?include_daos=true&?include_unpublished=false'
+export_options = '?numbered_cs=true&include_daos=true&include_unpublished=false'
 
 
 with open(digguide_input,'rb') as tsvin:
